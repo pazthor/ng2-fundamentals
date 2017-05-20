505faeb492a2abf6348a0356676447847679427f
Julio C. Pastor <final.africae@gmail.com>
2017-05-20 16:50:09 -0500

    Se agregan cambios a las rutas, se agrupan los componentes  ("barrels") y se mueve  el componente crear evento a la altura de events

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,20 +1,31 @@
 import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
+import {RouterModule, ActivatedRouteSnapshot} from '@angular/router'
+import{
+	EventsListComponent,
+	EventThumbnailComponent,
+	EventService,
+	EventDetailsComponent,
+	CreateEventComponent,
+	EventRouteActivator,
+	EventListResolver
+}from './events/index'
 
-import {RouterModule} from '@angular/router'
+
+ 
+
 import { EventsAppComponent } from  './events-app.component'
-import { EventsListComponent } from  './events/events-list.component'
-import { EventThumbnailComponent } from 
- './events/event-thumbnail.component'
 import { NavBarComponent } from  './nav/navbar.components'
-import {EventService}  from './events/shared/event.service'
 import {ToastrService}  from './commons/toastr.service'
-import {EventDetailsComponent} from './events/event-details/event-details.component'
 import {appRoutes}  from './routes'
-import { CreateEventComponent} from './events/shared/create-event.component'
 import {Error404Component} from './errors/404.component'
-import {EventRouteActivator} from './events/event-details/event-route-activator.service'
-import { EventListResolver } from './events/event-list-resolver.service'
+
+
+
+
+
+
+
 
 @NgModule({
 	imports: [BrowserModule, 
