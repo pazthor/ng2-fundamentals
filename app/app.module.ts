972ada7d9a1f7ba532b14f060d9986423f38d28a
Julio C. Pastor <final.africae@gmail.com>
2017-06-06 10:38:50 -0500

    se agrega componente collapsile-well

--- app/app.module.ts
+++ app/app.module.ts
@@ -23,6 +23,7 @@ import{
 import { EventsAppComponent } from  './events-app.component'
 import { NavBarComponent } from  './nav/navbar.components'
 import {ToastrService}  from './commons/toastr.service'
+import  {CollapsibleWellComponent}  from './commons/collapsible-well.component'
 import {appRoutes}  from './routes'
 import {Error404Component} from './errors/404.component'
 import {AuthService} from './user/auth.service'
@@ -50,7 +51,8 @@ import {AuthService} from './user/auth.service'
 		CreateEventComponent,
 		Error404Component,
 		CreateSessionComponent,
-		SessionListComponent
+		SessionListComponent,
+		CollapsibleWellComponent
 		],
 		providers:[
 			AuthService,
